f4c7f494cfff1a1fd012d09042dd7f3196704dfd
JoaoKeuwe <joaov2872@hotmail.com>
2023-12-09 14:55:20 -0300

    feat:

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -1,52 +1,18 @@
 import { Link } from "react-router-dom";
-import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 import "./chartBox.scss";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-const ChartBox = (props: any) => {
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
+
+const ChartBox = (props: Props) => {
   return (
     <div className="chartBox">
       <div className="boxInfo">
@@ -55,21 +21,23 @@ const ChartBox = (props: any) => {
           <span>{props.title}</span>
         </div>
         <h1>{props.number}</h1>
-        <Link to="">View All</Link>
+        <Link to="/" style={{ color: props.color }}>
+          View all
+        </Link>
       </div>
       <div className="chartInfo">
         <div className="chart">
           <ResponsiveContainer width="99%" height="100%">
-            <LineChart  data={props.chartData}>
+            <LineChart data={props.chartData}>
               <Tooltip
                 contentStyle={{ background: "transparent", border: "none" }}
                 labelStyle={{ display: "none" }}
-                position={{ x: 10, y: 80 }}
+                position={{ x: 10, y: 70 }}
               />
               <Line
                 type="monotone"
                 dataKey={props.dataKey}
-                stroke="#8884d8"
+                stroke={props.color}
                 strokeWidth={2}
                 dot={false}
               />
@@ -77,7 +45,12 @@ const ChartBox = (props: any) => {
           </ResponsiveContainer>
         </div>
         <div className="texts">
-          <span className="porcentage">45%</span>
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
           <span className="duration">this month</span>
         </div>
       </div>
